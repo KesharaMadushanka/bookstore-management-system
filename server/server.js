abd9a33df244c10cec105a9e7e94bf51681b7f46
Keshara Madushanka <122123096+KesharaMadushanka@users.noreply.github.com>
2023-05-31 22:34:08 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+//express
+const express = require('express');
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get('/', (req, res) => {
+  res.json({ mssg: 'Welcome' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('listening on port ' + process.env.PORT);
+});
